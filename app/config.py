079419c213c12a575e0880d13190438445f44a3c
linyuzhe <lingyuzhe210@gmail.com>
2023-10-24 00:15:00 +0800

    Fix Style

--- app/config.py
+++
@@ -1,8 +0,0 @@
-app_configuration: dict = {
-    "site_name": "Horizon Lin's Portfolio",
-    "theme": {
-        "font": "Gothic A1"
-    },
-    "copy_right": "Copyright © 2023. Horizon Lin",
-    "attributes": "Made with Reflex"
-}
